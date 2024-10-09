6b197dbd9f6b93716d6239dbb942d36e515a7e9f
Matth-Ben <matthiasbenoit.pro@gmail.com>
2024-10-09 23:53:02 +0200

    :construction: [HOME] Add image to video

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -14,12 +14,13 @@
             <p class="a text-center" v-for="paragraph in homepage.hero_description" :key="paragraph">
                 <span v-for="child in paragraph.children" :key="child" :class="{'text-secondary italic': child.italic}">
                     {{ child?.text }}
-                </span>
+                </span> 
             </p>
         </div>
 
         <div class="players" v-if="homepage.videos" ref="playersWrapper">
             <div class="player--fallow" v-for="(video, index) in homepage.videos" :key="video.id" ref="videoPlayers">
+                <img v-if="video.thumbnail?.url" :src="getImageUrl(video.thumbnail.url)" alt="{{ item.image.name }}" class="video-thumbnail w-full h-full">
                 <VimeoPlayer class="w-full h-full" :videoId="video.video_url" />
             </div>
         </div>
@@ -27,98 +28,104 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { gsap } from 'gsap'
-import VimeoPlayer from '~/components/VimeoPlayer.vue'
+    import { ref, onMounted } from 'vue'
+    import { gsap } from 'gsap'
+    import VimeoPlayer from '~/components/VimeoPlayer.vue'
 
-const homepage = ref({
-    hero_title: '',
-    hero_description: '',
-    videos: [],
-})
-
-const videoPlayers = ref([])
-const playersWrapper = ref(null)
-
-const mouseX = ref(0)
-const mouseY = ref(0)
-
-const handleMouseMove = (event) => {
-    mouseX.value = event.clientX
-    mouseY.value = event.clientY
-
-    videoPlayers.value.forEach((el, index) => {
-        const videoWidth = el.offsetWidth
-        const videoHeight = el.offsetHeight
-        const delay = index * 0.05
-
-        // Calculer les nouvelles positions
-        let newX = mouseX.value - videoWidth / 2
-        let newY = mouseY.value - videoHeight / 2
-
-        // Appliquer les contraintes pour éviter de sortir du wrapper
-        const wrapperRect = playersWrapper.value.getBoundingClientRect()
-        const maxX = wrapperRect.right - videoWidth
-        const maxY = wrapperRect.bottom - videoHeight
-        const minX = wrapperRect.left
-        const minY = wrapperRect.top
-
-        // Contrainte de position
-        newX = Math.max(minX, Math.min(newX, maxX))
-        newY = Math.max(minY, Math.min(newY, maxY))
-
-        // Appliquer les animations GSAP
-        gsap.to(el, {
-            x: newX - wrapperRect.left, // Translate par rapport à la position du wrapper
-            y: newY - wrapperRect.top, // Translate par rapport à la position du wrapper
-            zIndex: 5 - index,
-            delay,
-            ease: "expo.out",
-            duration: 0.4,
-        })
+    const homepage = ref({
+        hero_title: '',
+        hero_description: '',
+        videos: [],
     })
-}
 
-onMounted(async () => {
-    try {
-        const response = await $fetch('http://localhost:1337/api/homepage?populate[videos]=*')
-        const data = response.data
+    const videoPlayers = ref([])
+    const playersWrapper = ref(null)
 
-        homepage.value.hero_title = data.hero_title
-        homepage.value.hero_description = data.hero_description
-        homepage.value.videos = data.videos
-    } catch (error) {
-        console.error('Erreur lors de la récupération des données:', error)
+    const mouseX = ref(0)
+    const mouseY = ref(0)
+
+    const getImageUrl = (image) => {
+        return `http://localhost:1337${image}`
     }
-})
+
+    const handleMouseMove = (event) => {
+        mouseX.value = event.clientX
+        mouseY.value = event.clientY
+
+        videoPlayers.value.forEach((el, index) => {
+            const videoWidth = el.offsetWidth
+            const videoHeight = el.offsetHeight
+            const delay = index * 0.05
+
+            // Calculer les nouvelles positions
+            let newX = mouseX.value - videoWidth / 2
+            let newY = mouseY.value - videoHeight / 2
+
+            // Appliquer les contraintes pour éviter de sortir du wrapper
+            const wrapperRect = playersWrapper.value.getBoundingClientRect()
+            const maxX = wrapperRect.right - videoWidth
+            const maxY = wrapperRect.bottom - videoHeight
+            const minX = wrapperRect.left
+            const minY = wrapperRect.top
+
+            // Contrainte de position
+            newX = Math.max(minX, Math.min(newX, maxX))
+            newY = Math.max(minY, Math.min(newY, maxY))
+
+            // Appliquer les animations GSAP
+            gsap.to(el, {
+                x: newX - wrapperRect.left, // Translate par rapport à la position du wrapper
+                y: newY - wrapperRect.top, // Translate par rapport à la position du wrapper
+                zIndex: 5 - index,
+                delay,
+                ease: "expo.out",
+                duration: 0.4,
+            })
+        })
+    }
+
+    onMounted(async () => {
+        try {
+            const response = await $fetch('http://localhost:1337/api/homepage?populate[videos][populate]=thumbnail')
+            const data = response.data
+
+            homepage.value.hero_title = data.hero_title
+            homepage.value.hero_description = data.hero_description
+            homepage.value.videos = data.videos
+            console.log(homepage.value.videos);
+            
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données:', error)
+        }
+    })
 </script>
 
 <style scoped>
-img {
-    max-width: 100%;
-    height: auto;
-}
+    img {
+        max-width: 100%;
+        height: auto;
+    }
 
-.text-bigTitle {
-    font-size: 8rem;
-    line-height: 1;
-
-    @media screen and (min-width: 1024px) {
-        font-size: 18rem;
+    .text-bigTitle {
+        font-size: 8rem;
         line-height: 1;
 
-        span {
-            font-size: 19rem;
-            line-height: 19rem;
-        }   
+        @media screen and (min-width: 1024px) {
+            font-size: 18rem;
+            line-height: 1;
+
+            span {
+                font-size: 19rem;
+                line-height: 19rem;
+            }   
+        }
     }
-}
 
-.players {
-    @apply fixed z-10 w-full h-full overflow-hidden pt-24;
-}
+    .players {
+        @apply fixed z-10 w-full h-full overflow-hidden pt-24;
+    }
 
-.player--fallow {
-    @apply absolute w-[27.3rem] h-[15.8rem];
-}
+    .player--fallow {
+        @apply absolute w-[27.3rem] h-[15.8rem];
+    }
 </style>
